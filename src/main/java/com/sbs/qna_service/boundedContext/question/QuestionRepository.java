824de394c1280c8f5cf6db1a8b37671e82ae4c34
geongeongeon <leegeonhee0204@naver.com>
2024-12-19 21:23:30 +0900

    각 테스트 케이스의 독립성 확보

--- src/main/java/com/sbs/qna_service/boundedContext/question/QuestionRepository.java
+++ src/main/java/com/sbs/qna_service/boundedContext/question/QuestionRepository.java
@@ -1,6 +1,9 @@
 package com.sbs.qna_service.boundedContext.question;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,4 +14,10 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
 
     List<Question> findBySubjectLike(String subject);
+
+    @Modifying // INSERT, UPDATE, DELETE와 같이 데이터가 변경되는 작업에서만 사용
+    @Transactional
+    // nativeQuery = true여야 MySQL 쿼리 사용이 가능하다.
+    @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
+    void clearAutoIncrement();
 }
--- src/test/java/com/sbs/qna_service/QnaServiceApplicationTests.java
+++ src/test/java/com/sbs/qna_service/QnaServiceApplicationTests.java
@@ -2,6 +2,7 @@ package com.sbs.qna_service;
 
 import com.sbs.qna_service.boundedContext.question.Question;
 import com.sbs.qna_service.boundedContext.question.QuestionRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,15 +21,18 @@ class QnaServiceApplicationTests {
 	@Autowired // 필드 주입
 	private QuestionRepository questionRepository;
 
-	@Test
-	// DisplayName : 테스트의 의도를 사람이 읽기 쉬운 형태로 설명
-	@DisplayName("데이터 저장하기")
-	void t001() {
+	@BeforeEach // 테스트 케이스 실행 전에 딱 한번 실행
+	void beforeEach() {
+		// 모든 데이터 삭제
+		questionRepository.deleteAll();
+
+		// 흔적 삭제(다음 INSERT 때 Id가 1부터 시작하도록)
+		questionRepository.clearAutoIncrement();
+
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
 		q1.setCreateDate(LocalDateTime.now());
-		// save : INSERT 쿼리 실행
 		questionRepository.save(q1);  // 첫번째 질문 저장
 
 		Question q2 = new Question();
@@ -36,6 +40,20 @@ class QnaServiceApplicationTests {
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
 		questionRepository.save(q2);  // 두번째 질문 저장
+	}
+
+	@Test
+	// DisplayName : 테스트의 의도를 사람이 읽기 쉬운 형태로 설명
+	@DisplayName("데이터 저장하기")
+	void t001() {
+		Question q = new Question();
+		q.setSubject("겨울 제철 음식으로는 무엇을 먹어야 하나요?");
+		q.setContent("겨울 제철 음식을 알려주세요.");
+		q.setCreateDate(LocalDateTime.now());
+		// save : INSERT 쿼리 실행
+		questionRepository.save(q);  // 세번째 질문 저장
+
+		assertEquals("겨울 제철 음식으로는 무엇을 먹어야 하나요?", questionRepository.findById(3).get().getSubject());
 	}
 
 	// SQL : SELECT * FROM question;
