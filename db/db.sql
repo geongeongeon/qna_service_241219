490d7938128690c00d3ed80fda1808e77b5fd725
geongeongeon <leegeonhee0204@naver.com>
2024-12-19 20:56:44 +0900

    SQL SELECT

--- db/db.sql
+++ db/db.sql
@@ -3,12 +3,12 @@ DROP DATABASE IF EXISTS qna_service;
 CREATE DATABASE qna_service;
 USE qna_service;
 
-// 외래키 제약 비활성화
+# 외래키 제약 비활성화
 SET FOREIGN_KEY_CHECKS = 0;
 
-// TRUNCATE로 초기화한 데이터는 데이터 추가 시 1번부터 시작한다.
+# TRUNCATE로 초기화한 데이터는 데이터 추가 시 1번부터 시작한다.
 TRUNCATE question;
 TRUNCATE answer;
 
-// 외래키 제약 활성화
+# 외래키 제약 활성화
 SET FOREIGN_KEY_CHECKS = 1;
