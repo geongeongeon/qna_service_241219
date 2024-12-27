f0c19eb2e66d8c2e267f2825309408d6028fdbaa
geongeongeon <leegeonhee0204@naver.com>
2024-12-27 22:41:13 +0900

    타임리프 연결

--- src/main/java/com/sbs/qna_service/boundedContext/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sbs.qna_service.boundedContext.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// JpaRepository를 상속받아서 User 엔티티를 관리하는 리포지터리 생성
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
