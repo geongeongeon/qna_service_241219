a008d86833fb147de32074a68df95300640b0347
geongeongeon <leegeonhee0204@naver.com>
2024-12-27 21:59:36 +0900

    질문을 통해 답변 찾기

--- src/test/java/com/sbs/qna_service/QnaServiceApplicationTests.java
+++ src/test/java/com/sbs/qna_service/QnaServiceApplicationTests.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -183,6 +184,23 @@ class QnaServiceApplicationTests {
 		assertTrue(oa.isPresent());
 		Answer a = oa.get();
 		assertEquals(2, a.getQuestion().getId());
+	}
+
+	@Transactional // 테스트 코드에서는 Transactional을 붙여줘야 한다.
+	// findById 메서드를 실행하고 나면 DB 연결이 끊어진다.
+	// Transactional 어노테이션을 사용하면 메서드가 종료될 때까지 DB 연결이 유지된다.
+	@Test
+	@DisplayName("질문을 통해 답변 찾기")
+	void t011() {
+		// SQL : SELECT * FROM question WHERE id = 2;
+		Optional<Question> oq = questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 	}
 
 }
