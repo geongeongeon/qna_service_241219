b4c14b276b936a4122144d0af1e764b3158a7b76
geongeongeon <leegeonhee0204@naver.com>
2024-12-28 09:52:13 +0900

    답변 컨트롤러 만들기

---
+++ src/main/java/com/sbs/qna_service/boundedContext/answer/AnswerController.java
@@ -0,0 +1,25 @@
+package com.sbs.qna_service.boundedContext.answer;
+
+import com.sbs.qna_service.boundedContext.question.Question;
+import com.sbs.qna_service.boundedContext.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/answer")
+public class AnswerController {
+    private final QuestionService questionService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+        Question question = questionService.getQuestion(id);
+        // TODO: 답변을 저장한다.
+        return "redirect:/question/detail/%s".formatted(id);
+    }
+}
