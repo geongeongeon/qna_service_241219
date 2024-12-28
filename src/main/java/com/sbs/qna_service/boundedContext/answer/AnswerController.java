317f93434cd075812bc768fb2da0b237735ee33d
geongeongeon <leegeonhee0204@naver.com>
2024-12-28 10:33:40 +0900

    답변 저장하기

--- src/main/java/com/sbs/qna_service/boundedContext/answer/AnswerController.java
+++ src/main/java/com/sbs/qna_service/boundedContext/answer/AnswerController.java
@@ -5,10 +5,7 @@ import com.sbs.qna_service.boundedContext.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,9 +14,14 @@ public class AnswerController {
     private final QuestionService questionService;
 
     @PostMapping("/create/{id}")
+    // @ResponseBody
     public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
         Question question = questionService.getQuestion(id);
+
         // TODO: 답변을 저장한다.
+        Answer answer = questionService.create(question, content);
+
+        // return "%d번 질문에 대한 답변이 생성되었습니다.(답변 번호 : %d)".formatted(id, answer.getId());
         return "redirect:/question/detail/%s".formatted(id);
     }
 }
--- src/main/java/com/sbs/qna_service/boundedContext/question/QuestionService.java
+++ src/main/java/com/sbs/qna_service/boundedContext/question/QuestionService.java
@@ -1,9 +1,12 @@
 package com.sbs.qna_service.boundedContext.question;
 
+import com.sbs.qna_service.boundedContext.answer.Answer;
+import com.sbs.qna_service.boundedContext.answer.AnswerRepository;
 import com.sbs.qna_service.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +14,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class QuestionService {
     private final QuestionRepository questionRepository;
+    private final AnswerRepository answerRepository;
 
     public List<Question> findAll() {
         return questionRepository.findAll();
@@ -22,5 +26,15 @@ public class QuestionService {
         if(oq.isEmpty()) throw new DataNotFoundException("question not found");
 
         return oq.get();
+    }
+
+    public Answer create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        answerRepository.save(answer);
+
+        return answer;
     }
 }
