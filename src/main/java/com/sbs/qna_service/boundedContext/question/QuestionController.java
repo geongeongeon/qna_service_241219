a8a4d2ba7f89801f4a1cd77a1b754dfe9898a262
geongeongeon <leegeonhee0204@naver.com>
2024-12-28 09:16:42 +0900

    질문 상세 컨트롤러 만들기

--- src/main/java/com/sbs/qna_service/boundedContext/question/QuestionController.java
+++ src/main/java/com/sbs/qna_service/boundedContext/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -21,4 +22,8 @@ public class QuestionController {
         return "question_list";
     }
 
+    @GetMapping("/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        return "question_detail";
+    }
 }
