b3ea3d3c6aaa865642f203c6327ba61594cc7e35
geongeongeon <leegeonhee0204@naver.com>
2025-01-07 21:10:25 +0900

    로그인이 필요한 곳에서 로그인 여부 체크

--- src/main/java/com/sbs/qna_service/boundedContext/question/QuestionController.java
+++ src/main/java/com/sbs/qna_service/boundedContext/question/QuestionController.java
@@ -50,6 +50,7 @@ public class QuestionController {
     // @Valid QuestionForm questionForm
     // questionForm 값을 바인딩 할 때 유효성 체크를 해라!
     public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult, Principal principal) {
+        // Principal : 현재 인증된 사용자의 정보를 나타냄
         if (bindingResult.hasErrors()) { // hasErrors : 에러가 존재한다면 true, 존재하지 않다면 false
             // question_form.html 실행
             // 다시 작성하라는 의미로 응답에 폼을 실어서 보냄
