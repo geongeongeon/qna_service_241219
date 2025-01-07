b165fc492622d2708efc0236b7d34d699e47564b
geongeongeon <leegeonhee0204@naver.com>
2025-01-07 22:08:24 +0900

    답변 수정

--- src/main/java/com/sbs/qna_service/boundedContext/answer/AnswerService.java
+++ src/main/java/com/sbs/qna_service/boundedContext/answer/AnswerService.java
@@ -2,10 +2,12 @@ package com.sbs.qna_service.boundedContext.answer;
 
 import com.sbs.qna_service.boundedContext.question.Question;
 import com.sbs.qna_service.boundedContext.user.SiteUser;
+import com.sbs.qna_service.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +23,21 @@ public class AnswerService {
         answerRepository.save(answer);
 
         return answer;
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = answerRepository.findById(id);
+
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        answerRepository.save(answer);
     }
 }
