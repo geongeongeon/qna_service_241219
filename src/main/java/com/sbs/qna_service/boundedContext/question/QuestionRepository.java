8219cb9d3c45eb8a2f8ebb8e2c81fb476c0a9bd3
geongeongeon <leegeonhee0204@naver.com>
2025-01-13 17:01:14 +0900

    검색 테스트

--- src/main/java/com/sbs/qna_service/boundedContext/question/QuestionRepository.java
+++ src/main/java/com/sbs/qna_service/boundedContext/question/QuestionRepository.java
@@ -1,5 +1,8 @@
 package com.sbs.qna_service.boundedContext.question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -20,4 +23,6 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     // nativeQuery = true여야 MySQL 쿼리 사용이 가능하다.
     @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
     void clearAutoIncrement();
+
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
--- src/main/java/com/sbs/qna_service/boundedContext/question/QuestionService.java
+++ src/main/java/com/sbs/qna_service/boundedContext/question/QuestionService.java
@@ -1,13 +1,16 @@
 package com.sbs.qna_service.boundedContext.question;
 
+import com.sbs.qna_service.boundedContext.answer.Answer;
 import com.sbs.qna_service.boundedContext.answer.AnswerRepository;
 import com.sbs.qna_service.boundedContext.user.SiteUser;
 import com.sbs.qna_service.exception.DataNotFoundException;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -44,7 +47,7 @@ public class QuestionService {
         return q;
     }
 
-    public Page<Question> getList(int page) {
+    public Page<Question> getList(int page, String kw) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate")); // 작성일자 순으로 정렬
 
@@ -52,8 +55,9 @@ public class QuestionService {
         // page : 요청된 페이지 번호 (0부터 시작)
         // 10 : 한 페이지에 표시할 데이터 개수
         // Sort.by(sorts) : 정렬 기준, 'createDate'를 기준으로 정렬
-        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts)); // 한 페이지에 10개씩
-        return questionRepository.findAll(pageable);
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts)); // 한 페이지에 10개씩+
+        Specification<Question> spec = search(kw);
+        return questionRepository.findAll(spec, pageable);
     }
 
     public void modify(Question question, String subject, String content) {
@@ -70,5 +74,23 @@ public class QuestionService {
     public void vote(Question question, SiteUser siteUser) {
         question.addVoter(siteUser);
         questionRepository.save(question);
+    }
+
+    private Specification<Question> search(String kw) {
+        return new Specification<>() {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                query.distinct(true);  // 중복을 제거
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+                Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+                return cb.or(cb.like(q.get("subject"), "%" + kw + "%"), // 제목
+                        cb.like(q.get("content"), "%" + kw + "%"),      // 내용
+                        cb.like(u1.get("username"), "%" + kw + "%"),    // 질문 작성자
+                        cb.like(a.get("content"), "%" + kw + "%"),      // 답변 내용
+                        cb.like(u2.get("username"), "%" + kw + "%"));   // 답변 작성자
+            }
+        };
     }
 }
